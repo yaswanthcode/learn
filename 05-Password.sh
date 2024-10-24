6d556a861877b700a467e39dab31e249dcdb08a7
Yaswanthcode <yaswanth096@gmail.com>
2024-10-24 15:27:05 +0530

    password

---
+++ 05-Password.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " Enter username :"
+read USERNAME
+echo " Enter Password :"
+read PASSWORD
+
+echo " Username is $USERNAME "
+echo " Password is $PASSWORD "
+
+#writing script for taking password
