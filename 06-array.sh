86d5a5c667e4420e63394be3f78c6b688d7dea7a
Yaswanthcode <yaswanth096@gmail.com>
2024-10-24 15:40:44 +0530

    array script

--- 06-array.sh
+++ 06-array.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("RRR" "Rangasthalam" "Magadheera")
 
-echo " First movie name is : ${MOVIES[0]}
-echo " second movie name is : ${MOVIES[1]}
-echo " Third movie name is : ${MOVIES[2]}
+echo " First movie name is : ${MOVIES[0]} "
+echo " second movie name is: ${MOVIES[1]} "
+echo " Third movie name is : ${MOVIES[2]} "
 
-echo " All best movies of ramcharan : ${MOVIES[@]}
+echo " All best movies of ramcharan : ${MOVIES[@]} "
