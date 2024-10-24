8a9753015c76803bb5649bf2770e5b3b9b3e13f9
Yaswanthcode <yaswanth096@gmail.com>
2024-10-24 15:10:14 +0530

    ADD COMMIT

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,9 +4,9 @@ PERSON1=shantanu
 PERSON2=tata
 
 echo   "    $PERSON1 : Hello "
-echo    "   $PERSON2 : Hi $PERSON1 "
+echo   "    $PERSON2 : Hi $PERSON1 "
 
 echo " $PERSON1: how are you ? "
-
+echo " $PERSON2: FINE  ,"
 
 
