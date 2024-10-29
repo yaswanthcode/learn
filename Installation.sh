d21188b203c9229700469b0581e40ace13a04ddf
Yaswanthcode <yaswanth096@gmail.com>
2024-10-29 15:13:48 +0530

    installation

--- Installation.sh
+++ Installation.sh
@@ -2,11 +2,10 @@
 
 USERID=$(id -u)
 echo "$USERID "
-if [ $USERID -ne 0]
-then 
+if [$USERID -ne 0]
     echo " This user is not a superuser "
 else
-    echo -e /e[32m "This user is a super user "
+    echo -e \e[32m "This user is a super user "
 fi 
 
 dnf install mysql -y 
