f063e56185da8cd640ab1ed64a9759e0d20b8ba3
Yaswanthcode <yaswanth096@gmail.com>
2024-10-29 14:31:52 +0530

    shell script file chnges

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 PERSON1=shantanu
-PERSON2=tata
+PERSON2=Tata
 
-echo   "    $PERSON1 : Hello "
-echo   "    $PERSON2 : Hi $PERSON1 "
+echo   "$PERSON1 : Hello "
+echo   "$PERSON2 : Hi $PERSON1 "
 
-echo " $PERSON1: how are you ? "
-echo " $PERSON2: FINE  ,"
+echo "$PERSON1: how are you ?"
+echo "$PERSON2: FINE, How about you?"
+
+echo "$PERSON1:good $PERSON2."
+echo "$PERSON2: Catchup later"
 
 
 
