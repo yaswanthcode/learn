de14b7e9ad6d2639de510430a60b872b12c1a58b
Yaswanthcode <yaswanth096@gmail.com>
2024-10-29 14:35:48 +0530

    shell script file chnges

--- 04-args.sh
+++ 04-args.sh
@@ -3,7 +3,8 @@
 PERSON1=$1 
 PERSON2=$2 
 
-echo " $PERSON1 "
-echo " $PERSON2 "
+echo "Taking parameters from arguments"
+echo "This is the first person name $PERSON1 "
+echo "This is the second person name $PERSON2 "
 
 #here we are trying to pass parameters in commandline 
