0b73c5b8fa16140a78649a1ecfdacd9a2bdfcbdf
Yaswanthcode <yaswanth096@gmail.com>
2024-10-29 15:25:40 +0530

    installation

--- Installation.sh
+++ Installation.sh
@@ -6,7 +6,8 @@ echo "$USERID "
 
 if [ $USERID -ne 0 ] 
 then 
-    echo " \e[31m Please run this programme with root access"
+    echo "\e[31mPlease run this programme with root access \e[0m"
+    exit 1
 else
     echo  -e " \e[32m This user is a super user"
 fi 
