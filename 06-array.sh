1e8bec97095a3b2247d79340604ce9344b49ebf2
Yaswanthcode <yaswanth096@gmail.com>
2024-10-29 15:04:16 +0530

    installation

--- 06-array.sh
+++ 06-array.sh
@@ -5,7 +5,6 @@ MOVIES=("RRR" "Rangasthalam" "Magadheera")
 echo " First movie name is : ${MOVIES[0]} "
 echo " second movie name is: ${MOVIES[1]} "
 echo " Third movie name is : ${MOVIES[2]} "
-
 echo " All best movies of ramcharan : ${MOVIES[@]} "
 
 echo " Sript name is : $0 "
