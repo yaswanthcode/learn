f9f695132f17a7b9bad2104cf564e6b9325753eb
Yaswanthcode <yaswanth096@gmail.com>
2024-11-21 20:39:29 +0530

    Commiting

--- 16-word-count.sh
+++ 16-word-count.sh
@@ -2,8 +2,8 @@
 
 SOURCE_FILE="/learn/doc.txt"
 
-Lines=' wc --lines < $SOURCE_FILE '
-words=' wc --word < $SOURCE_FILE '
+no_lines=' wc --lines < $SOURCE_FILE '
+no_words=' wc --word < $SOURCE_FILE '
 
-echo "No of lines : $Lines "
-echo "No of words : $words "
+echo "No of lines : $no_lines "
+echo "No of words : $n0_words "
