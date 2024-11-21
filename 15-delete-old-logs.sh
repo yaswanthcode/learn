a32bccd3e64808b9c8f72b2b1897a3538391396d
Yaswanthcode <yaswanth096@gmail.com>
2024-11-21 19:37:57 +0530

    Commiting

---
+++ 15-delete-old-logs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+id [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e " $G Directory exists $N "
+else
+    echo -e " $R Check the directory name $N "
+fi
