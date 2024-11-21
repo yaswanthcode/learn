a884f5794adf3feb6a83adb4e79eb12695d413ea
Yaswanthcode <yaswanth096@gmail.com>
2024-11-21 19:05:35 +0530

    Commiting

--- 12-current-script.sh
+++ 12-current-script.sh
@@ -5,7 +5,8 @@ COURSE="current script"
 echo " This text is from $COURSE "
 echo " PID $$ "
 
-./13-after-calling-script.sh
+#./13-after-calling-script.sh
+source ./13-after-calling-script.sh
 
 echo "This text is from $COURSE "
 echo " PID $$ "
