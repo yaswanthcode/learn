a0f9fdbb63c86295d791ef82aef8a3eb24dbe4b9
yaswanthcode <yaswanthk97@gmail.com>
2024-11-25 22:22:30 +0530

    commiting new chnages

--- 17-disk-uasage.sh
+++ 17-disk-uasage.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 DISK=$(df -hT | grep xfs)
+DISK_FULL=4
 
-echo " $DISK "
+while IFS = read -r line 
+do
+    USAGE=$(echo $line | awk -F " " { print $6F}| cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " {print $NF})
+    if [ $USAGE -ge $DISK_FULL ]
+    then
+        MESSAGE+=$( $FOLDER IS MORE THAN $DISK_FULL , Current usage : $USAGE)
+    fi 
+
+done
+
+
+echo " $MESSAGE "
