0c892840cae090ff5dc0f107a8369b3fbcdba994
yaswanthcode <yaswanthk97@gmail.com>
2024-11-25 23:21:18 +0530

    commiting new chnages

--- 17-disk-uasage.sh
+++ 17-disk-uasage.sh
@@ -10,7 +10,7 @@ do
    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
    FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [$USAGE -ge $DISK_FULL];
-    then
+     then
         MESSAGE+="$FOLDER IS MORE THAN $DISK_FULL , Current usage : $USAGE \n"
     fi 
 
