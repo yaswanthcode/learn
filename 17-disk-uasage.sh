40609d45a82f8656c2fd7570c79d24c342fd3c90
yaswanthcode <yaswanthk97@gmail.com>
2024-11-25 23:22:33 +0530

    commiting new chnages

--- 17-disk-uasage.sh
+++ 17-disk-uasage.sh
@@ -9,11 +9,13 @@ while IFS= read -r line
 do
    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [$USAGE -ge $DISK_FULL];
-     then
+    if [$USAGE -ge $DISK_FULL]
         MESSAGE+="$FOLDER IS MORE THAN $DISK_FULL , Current usage : $USAGE \n"
     fi 
 
 done <<< $DISK_U
 
 echo -e " $MESSAGE "
+if [ "$(command -v command)" ]; then
+    echo "command \"command\" exists on system"
+fi
