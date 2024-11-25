f39481f5403b800bd3de3c669b14d3ff113860c9
yaswanthcode <yaswanthk97@gmail.com>
2024-11-25 23:19:09 +0530

    commiting new chnages

--- 17-disk-uasage.sh
+++ 17-disk-uasage.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
 
 
-#DISK=$(df -hT | grep xfs)
-#DISK_FULL=4
-#MESSAGE=""
-
-#while IFS= read -r line 
-#do
- #   USAGE=$(echo $line | awk -F " " '{ print $6F }' | cut -d "%" -f1)
- #   FOLDER=$(echo $line | awk -F " " '{ print $NF }')
-  #  if [ $USAGE -ge $DISK_FULL] 
-  #  then
-  #      MESSAGE+="$FOLDER IS MORE THAN $DISK_FULL , Current usage : $USAGE"
-  #  fi 
-
-#done <<< $DISK
-
-#echo " $MESSAGE "
-
-#!/bin/bash
-
-DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=4
+DISK_U=$(df -hT | grep xfs)
+DISK_FULL=4
 MESSAGE=""
 
-while IFS= read -r line
+while IFS= read -r line 
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
-    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+   FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_FULL] 
     then
-        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
-    fi
-done <<< $DISK_USAGE
+        MESSAGE+="$FOLDER IS MORE THAN $DISK_FULL , Current usage : $USAGE \n"
+    fi 
 
-echo -e "Message: $MESSAGE"
+done <<< $DISK_U
 
-#echo "$MESSAGE" | mail -s "Disk Usage Alert" info@joindevops.com
-
-# echo "body" | mail -s "subject" to-address
+echo -e " $MESSAGE "
