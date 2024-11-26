584ab91db8cb82c5e36dad4d4878e0101173664c
yaswanthcode <yaswanthk97@gmail.com>
2024-11-26 10:21:17 +0530

    message of commit

--- 17-disk-uasage.sh
+++ 17-disk-uasage.sh
@@ -9,7 +9,8 @@ while IFS= read -r line
 do
    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [$USAGE -ge $DISK_FULL]
+    if [ $USAGE -ge $DISK_FULL ]
+    then
         MESSAGE+="$FOLDER IS MORE THAN $DISK_FULL , Current usage : $USAGE \n"
     fi 
 
@@ -19,3 +20,25 @@ echo -e " $MESSAGE "
 if [ "$(command -v command)" ]; then
     echo "command \"command\" exists on system"
 fi
+
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk Usage Alert" info@joindevops.com
+
+# echo "body" | mail -s "subject" to-address
